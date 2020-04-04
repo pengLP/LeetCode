f156b2076d79ba1572a77a0749c231ca7a3d35fa
pengLp <761079834@qq.com>
2020-04-04 21:13:16 +0800

    上传代码

--- java/test/Main.java
+++ java/test/Main.java
@@ -6,12 +6,10 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-        List<Integer> list = new ArrayList<>();
-        int i = 0;
-        while (true) {
-            list.add(i++);
-            System.out.println(i);
-        }
+        HashMap<Integer , Integer> map = new HashMap<>();
+        map.put(1 , 1);
+        System.out.println(map.get(1));
+        
     }
 
 }
